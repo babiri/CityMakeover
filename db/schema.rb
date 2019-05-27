809b316a8d764220ff34ce3121bb84142c5d8f21
Pedro Silva <pedrofjrsilva@gmail.com>
2019-05-27 17:45:28 +0100

    changed schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,28 @@ ActiveRecord::Schema.define(version: 2019_05_27_134929) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fixpoints", force: :cascade do |t|
+    t.boolean "fixed"
+    t.date "creation_date"
+    t.date "fix_date"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "photo"
+    t.boolean "fixed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,6 +48,11 @@ ActiveRecord::Schema.define(version: 2019_05_27_134929) do
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
