bbda37eb8b1b6442514d3254a09ca413fac5131e
luis quilho <luis.quilho@gmail.com>
2019-05-27 14:58:04 +0100

    created 10 fake user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database"
+puts 'Creating fake users...'
+
+10.times do
+  user = User.new(
+    name: Faker::Name.name ,
+    email: Faker::Internet.email,
+    password: "123456"
+    )
+  user.save!
+end
+puts "finished creating users"
+
+# puts "creating fake "
+
