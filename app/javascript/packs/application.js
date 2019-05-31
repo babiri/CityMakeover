457b7e20bf5e8120778218e6807c95cc770ff195
Fermin Mata <fermenmata@gmail.com>
2019-05-31 13:03:26 +0100

    worked on the toggle button for index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,3 +7,22 @@ import { initMapbox } from '../plugins/init_mapbox';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 initMapbox();
+
+
+if(document.querySelector(".fixpoints.index")) {
+  let toggleButton = document.querySelector("#toggle-list");
+  let fixpointsList = document.querySelector("#fixpoints-list");
+  let map = document.querySelector("#map");
+  let toggleIcon = document.querySelector("#toggle-list i");
+  // when the toggle is clicked
+  // 1. remove d-none class from index
+  // 2. add d-none to the map div
+  // 3. change the class of the toggle button to on
+
+  toggleButton.addEventListener("click", (event) => {
+    map.classList.add("d-none");
+    fixpointsList.classList.remove("d-none");
+    toggleIcon.classList.remove("fa-toggle-off");
+    toggleIcon.classList.add("fa-toggle-on");
+  });
+}
