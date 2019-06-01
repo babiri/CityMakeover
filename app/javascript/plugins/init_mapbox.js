5d888267d89b6e1dc6d2a81ef20dd9ecbf1d2a06
Pedro Silva <pedrofjrsilva@gmail.com>
2019-06-01 11:21:04 +0100

    minor changes in map theme

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -30,7 +30,6 @@ const addMarkersToMap = (map, markers) => {
     new mapboxgl.Marker(element)
       .setLngLat([marker.lng, marker.lat])
       .setPopup(popup)
-
       .addTo(map);
   });
 };
@@ -38,7 +37,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 75 });
 };
 
 const initMapbox = () => {
