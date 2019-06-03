020132ffa7b7828e777d9c703512a8da83421f1a
luis quilho <luis.quilho@gmail.com>
2019-06-03 12:55:18 +0100

    upvoting ajax done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,8 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || root_path
+
+  end
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,16 +1,15 @@
 class VotesController < ApplicationController
-
   before_action :get_fixpoint, only: :create
 
   def create
-    vote = Vote.create(fixpoint: @fixpoint, user: current_user)
-    authorize votes
-    render json: vote
+    @vote = Vote.create(fixpoint: @fixpoint, user: current_user)
+    authorize @vote
+    # render json: vote
   end
 
   def destroy
-    vote = current_user.votes.find(params[:id])
-    authorize vote
+    @vote = current_user.votes.find(params[:id])
+    authorize @vote
   end
 
   private
