7089e8b834d0463171018705163816a3302ff6f6
luis quilho <luis.quilho@gmail.com>
2019-06-05 11:10:16 +0100

    changed error messages

--- app/models/fixpoint_attachment.rb
+++ app/models/fixpoint_attachment.rb
@@ -1,15 +1,3 @@
-# == Schema Information
-#
-# Table name: fixpoint_attachments
-#
-#  id          :bigint           not null, primary key
-#  photo       :string
-#  fixpoint_id :bigint
-#  fixed       :boolean
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#
-
 class FixpointAttachment < ApplicationRecord
   belongs_to :fixpoint
   mount_uploader :photo, PhotoUploader
