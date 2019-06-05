cba4a9ca0ef9d25538466657170941c716c0866f
Andre Pires <andrecabralpires@gmail.com>
2019-06-05 12:03:23 +0100

    autocomplete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,10 @@ import { initMapbox } from '../plugins/init_mapbox';
 
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
+import { initAutocomplete } from '../plugins/init_autocomplete';
+
 initMapbox();
+initAutocomplete();
 
 
 if(document.querySelector(".fixpoints.index")) {
